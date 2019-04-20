733c9e5ea76640720623e433d669174df6b9af93
Michelle King <michellelking@me.com>
2019-04-20 13:09:06 -0500

    Fixed pressure cooker recipes

---
+++ pressurecook/nav-bar.js
@@ -0,0 +1,16 @@
+export default {
+    template: `
+    <aside id="fh5co-aside" role="complementary" class="border js-fullheight">
+      <h1 id="fh5co-logo"><a href="../index.html"><img src="../images/logo-colored.png" alt=""></a></h1>
+      <nav id="fh5co-main-menu" role="navigation">
+      <ul>
+      <li><a href="../index.html">Cookbook</a></li>
+      <li class="fh5co-active"><a href="../pressurecook.html">Pressure Cooker</a></li>
+      <li><a href="../Restaurants.html">Restaurant Food</a></li>
+      <li><a href="https://michellelking.github.io">My Homepage</a></li>
+      </ul>	
+      </nav>
+    </aside>
+    `
+  }
+
